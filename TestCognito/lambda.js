2d44618c6813c7e8c6c41c6b6fa158b6f8583073
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-26 16:26:09 +0530

    add cognito trigger post confirmation

--- TestCognito/lambda.js
+++ TestCognito/lambda.js
@@ -1,17 +1,8 @@
 let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 exports.handler = function (event, context, callback) {
-	cognito_idp.listUsers({
-		UserPoolId: process.env.UserPoolId_cognitoCognitoResource,
-		AttributesToGet: '[address,birthdate,email,gender,name,phone_number]'
-	}, function (error, data) {
-		if (error) {
-			// implement error handling logic here
-			throw error;
-		}
-		// your logic goes within this block
-	});
-
+	console.log(event);
+	
 
 	callback(null, 'Successfully executed');
 }
