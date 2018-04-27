438dcf1908d6871968cb6373e1d647c09299c719
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-27 09:47:11 +0530

    custom message cognito trigger

--- TestCognito/lambda.js
+++ TestCognito/lambda.js
@@ -2,7 +2,7 @@ let AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 exports.handler = function (event, context, callback) {
 	console.log(event);
-	
+		
 
 	callback(null, 'Successfully executed');
 }
